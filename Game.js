b5fb6a8b88ae8a5d3f6a53db7bbaf1ed2225652a
judeallanhill <judeallanhill@icloud.com>
2023-01-03 15:29:20 -0800

    Adding Skin Rectangles for skin selector

--- Game.js
+++ Game.js
@@ -1,4 +1,8 @@
-import {start_particles, draw_particles, update_particles} from "./particles.js"
+import {
+  start_particles,
+  draw_particles,
+  update_particles,
+} from "./particles.js";
 const c = document.getElementById("Game");
 const ctx = c.getContext("2d");
 const splashScreen = document.querySelector(".splash");
@@ -6,13 +10,15 @@ const shake_enabled = true;
 const PlayerPaddleSkin = new Image();
 const PaddelSkinAI = new Image();
 const TopHatSkin = new Image();
-TopHatSkin.src = "/TopHarSkin.png"
-let TopHat = "/TopHatSkin.png"
-let ChristmasHat = "/ChristMasSkin.png"
-PaddelSkinAI.src = "/PlainSkin.png"
-PlayerPaddleSkin.src = TopHat
+const ChristmasHatSkin = new Image();
+TopHatSkin.src = "/TopHatSkin.png";
+ChristmasHatSkin.src = "/ChristMasSkin.png";
+let TopHat = "/TopHatSkin.png";
+let ChristmasHat = "/ChristMasSkin.png";
+PaddelSkinAI.src = "/PlainSkin.png";
+PlayerPaddleSkin.src = TopHat;
 
-let paused = false
+let paused = false;
 const DEBUG = {
   print_ai: false,
 };
@@ -57,207 +63,260 @@ let current_keys = new Map();
 ctx.font = "50px pacifico_font";
 ctx.fillStyle = "#f0f6f0";
 let FlipedControlles = false;
-let settingsWindowOpen = false
+let settingsWindowOpen = false;
 let settingRuned = false;
 
 let splashScreenOn = false;
 let BALLLIVE = false;
 let levelLeftOfset = 100;
 let level1 = {
-x: 10,
-y: 98,
-w: 50,
-h: 55,
+  x: 10,
+  y: 98,
+  w: 50,
+  h: 55,
 };
 let level2 = {
-x: 110,
-y: 98,
-w: 50,
-h: 50,
-
+  x: 110,
+  y: 98,
+  w: 50,
+  h: 50,
 };
 
 let level3 = {
-x: 210,
-y: 98,
-w: 50,
-h: 50,
-
+  x: 210,
+  y: 98,
+  w: 50,
+  h: 50,
 };
 let RectWidthLine = 5;
 let RectOfset = 40;
 let TextLeftOfset = 250;
 let OutlineOfset = 225;
-let RectLeftOfset =  250;
+let RectLeftOfset = 250;
 let RectHeight = 100;
 let levelOn = 1;
 let LevelStart = false;
-function StartLevelSelect(ctx,c) {
-if (LevelStart === false) {
-  LevelStart = true;
+function StartLevelSelect(ctx, c) {
+  if (LevelStart === false) {
+    LevelStart = true;
+  }
 }
-}
-function updateLevelSelect(ctx,c) {
-if (ball.alive === false) {
-  if ((LevelStart === true)) {
-    console.log("LevelSelect");
-    ctx.fillStyle = "gray";
-    ctx.fillRect(0, 0, c.width, c.height);
-    //DrawOutline
-    if (levelOn === 1) {
-      ctx.lineWidth = RectWidthLine;
-      ctx.strokeStyle = "red";
-      ctx.strokeRect(level1.x + OutlineOfset, level1.y, level1.w, level1.h);
-    }
-    if (levelOn === 2) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("2");
-      ctx.strokeRect(level2.x + OutlineOfset, level2.y, level1.w, level1.h);
-    }
-    if (levelOn === 3) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("3");
-      ctx.strokeRect(level3.x + OutlineOfset, level3.y, level1.w, level1.h);
-    }
-    if (levelOn === 4) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("4");
-      ctx.strokeRect(310 + OutlineOfset, 98, 50, 55);
-    }
-
-    if (levelOn === 5) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("5");
-      ctx.strokeRect(410 + OutlineOfset, 98, 50, 55);
-    }
-    if (levelOn === 6) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("6");
-      ctx.strokeRect(10 + OutlineOfset, 198, 50, 55);
-    }
-    if (levelOn === 7) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("7");
-      ctx.strokeRect(110 + OutlineOfset, 198, 50, 55);
-    }
-    if (levelOn === 8) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("8");
-      ctx.strokeRect(210 + OutlineOfset, 198, 50, 55);
-    }
-    if (levelOn === 9) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("9");
-      ctx.strokeRect(310 + OutlineOfset, 198, 50, 55);
-    }
-    if (levelOn === 10) {
-      ctx.lineWidth = RectWidthLine;
-      console.log("10");
-      ctx.strokeRect(410 + OutlineOfset, 198, 50, 55);
-    }
-
-    //Draw Rects
-    ctx.font = "48px serif";
-    ctx.fillStyle = "blue";
-    //Number 1 Rect
-    ctx.fillRect(-15 + RectLeftOfset, RectHeight, 50, 50);
-    //Number 2 Rect
-    for (let i = 0; i < 400; i += 100) {
-      ctx.fillRect(75 + RectLeftOfset + i + 10, RectHeight, 50, 50);
-    }
-    ctx.fillRect(-15 + RectLeftOfset, RectHeight + 100, 50, 50);
-    for (let i = 0; i < 400; i += 100) {
-      ctx.fillRect(75 + RectLeftOfset + i + 10, RectHeight + 100, 50, 50);
-    }
-    ctx.fillStyle = "white";
-    ctx.fillText("1", 0 + TextLeftOfset, RectHeight + RectOfset);
-    ctx.fillText("2", 100 + TextLeftOfset, RectHeight + RectOfset);
-    ctx.fillText("3", 200 + TextLeftOfset, RectHeight + RectOfset);
-    ctx.fillText("4", 300 + TextLeftOfset, RectHeight + RectOfset);
-    ctx.fillText("5", 400 + TextLeftOfset, RectHeight + RectOfset);
-    ctx.fillText("6", 0 + TextLeftOfset, 240);
-    ctx.fillText("7", 100 + TextLeftOfset, 240);
-    ctx.fillText("8", 200 + TextLeftOfset, 240);
-    ctx.fillText("9", 300 + TextLeftOfset, 240);
-    ctx.fillText("10", 385 + TextLeftOfset, 240);
-
-    if (splashScreenOn === true) {
-      if (current_keys.get("Enter") === true) {
-        if (levelOn === 1) {
-          BALLLIVE = true;
-          AISpeed = 2;
-        }
-        if (levelOn === 2) {
-          BALLLIVE = true;
-          AISpeed = 4;
-        }
-        if (levelOn === 3) {
-          BALLLIVE = true;
-          AISpeed = 6;
-        }
-        if (levelOn === 4) {
-          BALLLIVE = true;
-          AISpeed = 8;
-        }
-        if (levelOn === 5) {
-          BALLLIVE = true;
-          AISpeed = 10;
-        }
-        if (levelOn === 6) {
-          BALLLIVE = true;
-          AISpeed = 12;
-        }
-        if (levelOn === 7) {
-          BALLLIVE = true;
-          AISpeed = 14;
-        }
-        if (levelOn === 8) {
-          BALLLIVE = true;
-          AISpeed = 16;
-        }
-        if (levelOn === 9) {
-          BALLLIVE = true;
-          AISpeed = 18;
-        }
-        if (levelOn === 10) {
-          BALLLIVE = true;
-          AISpeed = 20;
-        }
+function updateLevelSelect(ctx, c) {
+  if (ball.alive === false) {
+    if (LevelStart === true) {
+      ctx.fillStyle = "gray";
+      ctx.fillRect(0, 0, c.width, c.height);
+      //DrawOutline
+      if (levelOn === 1) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(level1.x + OutlineOfset, level1.y, level1.w, level1.h);
       }
-      if (BALLLIVE === true) {
-        ball.alive = true;
+      if (levelOn === 2) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(level2.x + OutlineOfset, level2.y, level1.w, level1.h);
+      }
+      if (levelOn === 3) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(level3.x + OutlineOfset, level3.y, level1.w, level1.h);
+      }
+      if (levelOn === 4) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(310 + OutlineOfset, 98, 50, 55);
+      }
+
+      if (levelOn === 5) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(410 + OutlineOfset, 98, 50, 55);
+      }
+      if (levelOn === 6) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(10 + OutlineOfset, 198, 50, 55);
+      }
+      if (levelOn === 7) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(110 + OutlineOfset, 198, 50, 55);
+      }
+      if (levelOn === 8) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(210 + OutlineOfset, 198, 50, 55);
+      }
+      if (levelOn === 9) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(310 + OutlineOfset, 198, 50, 55);
+      }
+      if (levelOn === 10) {
+        ctx.lineWidth = RectWidthLine;
+        ctx.strokeRect(410 + OutlineOfset, 198, 50, 55);
+      }
+
+      //Draw Rects
+      ctx.font = "48px serif";
+      ctx.fillStyle = "blue";
+      //Number 1 Rect
+      ctx.fillRect(-15 + RectLeftOfset, RectHeight, 50, 50);
+      //Number 2 Rect
+      for (let i = 0; i < 400; i += 100) {
+        ctx.fillRect(75 + RectLeftOfset + i + 10, RectHeight, 50, 50);
+      }
+      ctx.fillRect(-15 + RectLeftOfset, RectHeight + 100, 50, 50);
+      for (let i = 0; i < 400; i += 100) {
+        ctx.fillRect(75 + RectLeftOfset + i + 10, RectHeight + 100, 50, 50);
+      }
+      ctx.fillStyle = "white";
+      ctx.fillText("1", 0 + TextLeftOfset, RectHeight + RectOfset);
+      ctx.fillText("2", 100 + TextLeftOfset, RectHeight + RectOfset);
+      ctx.fillText("3", 200 + TextLeftOfset, RectHeight + RectOfset);
+      ctx.fillText("4", 300 + TextLeftOfset, RectHeight + RectOfset);
+      ctx.fillText("5", 400 + TextLeftOfset, RectHeight + RectOfset);
+      ctx.fillText("6", 0 + TextLeftOfset, 240);
+      ctx.fillText("7", 100 + TextLeftOfset, 240);
+      ctx.fillText("8", 200 + TextLeftOfset, 240);
+      ctx.fillText("9", 300 + TextLeftOfset, 240);
+      ctx.fillText("10", 385 + TextLeftOfset, 240);
+
+      if (splashScreenOn === true) {
+        if (current_keys.get("Enter") === true) {
+          if (levelOn === 1) {
+            BALLLIVE = true;
+            AISpeed = 2;
+          }
+          if (levelOn === 2) {
+            BALLLIVE = true;
+            AISpeed = 4;
+          }
+          if (levelOn === 3) {
+            BALLLIVE = true;
+            AISpeed = 6;
+          }
+          if (levelOn === 4) {
+            BALLLIVE = true;
+            AISpeed = 8;
+          }
+          if (levelOn === 5) {
+            BALLLIVE = true;
+            AISpeed = 10;
+          }
+          if (levelOn === 6) {
+            BALLLIVE = true;
+            AISpeed = 12;
+          }
+          if (levelOn === 7) {
+            BALLLIVE = true;
+            AISpeed = 14;
+          }
+          if (levelOn === 8) {
+            BALLLIVE = true;
+            AISpeed = 16;
+          }
+          if (levelOn === 9) {
+            BALLLIVE = true;
+            AISpeed = 18;
+          }
+          if (levelOn === 10) {
+            BALLLIVE = true;
+            AISpeed = 20;
+          }
+        }
+        if (BALLLIVE === true) {
+          ball.alive = true;
+        }
       }
     }
   }
 }
-}
-
+let LeftPressed = false;
+let RightPressed = false;
+let skinOn = 1;
 function draw_settings_window() {
-    if (settingsWindowOpen === true) {
-        ctx.fillStyle = "red"
-        ctx.fillRect(0,0,c.width,c.height)
-        ctx.strokeStyle = "black"
-        let ofset = 10;
-        for (let i = 0; i < 5; i += 1) {
-          ofset = ofset + 150;
-          ctx.strokeRect(-10 + ofset,75,100,100)
-        }
-        ctx.imageSmoothingEnabled = false
-        ctx.drawImage(PlayerPaddleSkin, 190, 80,14*1.2,70*1.2)
-        ctx.drawImage(PlayerPaddleSkin, 190, 80,14*1.2,70*1.2)
+  if (settingsWindowOpen === true) {
+    ctx.imageSmoothingEnabled = false;
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, c.width, c.height);
+    ctx.fillStyle = "blue";
+    let ofset = 10;
+    for (let i = 0; i < 5; i += 1) {
+      ofset = ofset + 150;
+      ctx.fillRect(-10 + ofset, 75, 100, 100);
+    }
 
+    if (current_keys.get("ArrowLeft") === true) {
+      if (LeftPressed === false) {
+        skinOn -= 1;
+        LeftPressed = true;
+        RightPressed = false;
       }
+    }
+    if (current_keys.get("ArrowLeft") === false) {
+      LeftPressed = false;
+    }
+    if (current_keys.get("ArrowRight") === true) {
+      if (RightPressed === false) {
+        skinOn += 1;
+        RightPressed = true;
+        LeftPressed = false;
+      }
+    }
+    if (current_keys.get("ArrowRight") === false) {
+      RightPressed = false;
+    }
+    console.log(skinOn);
+    if (skinOn === 1) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(145, 70, 110, 110);
+    }
+    if (skinOn === 2) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(295, 70, 110, 110);
+    }
+    if (skinOn === 3) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(145 + 150 * skinOn - 150, 70, 110, 110);
+    }
+    if (skinOn === 4) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(145 + 150 * skinOn - 150, 70, 110, 110);
+    }
+    if (skinOn === 5) {
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(145 + 150 * skinOn - 150, 70, 110, 110);
+    }
+    // if (skinOn === 6) {
+    //   ctx.strokeStyle = "black"
+    //   ctx.strokeRect (145 + 150*skinOn-150,70,110,110)
+    // }
+    // if (skinOn === 7) {
+    //   ctx.strokeStyle = "black"
+    //   ctx.strokeRect (145 + 150*skinOn-150,70,110,110)
+    // }
+    // if (skinOn === 8) {
+    //   ctx.strokeStyle = "black"
+    //   ctx.strokeRect (145 + 150*skinOn-150,70,110,110)
+    // }
+    // if (skinOn === 9) {
+    //   ctx.strokeStyle = "black"
+    //   ctx.strokeRect (145 + 150*skinOn-150,70,110,110)
+    // }
+    // if (skinOn === 10) {
+    //   ctx.strokeStyle = "black"
+    //   ctx.strokeRect (145 + 150*skinOn-150,70,110,110)
+    // }
+    ctx.drawImage(TopHatSkin, 190, 80, 14 * 1.2, 70 * 1.2);
+    ctx.drawImage(ChristmasHatSkin, 340, 80, 14 * 1.2, 70 * 1.2);
+    if (current_keys.get("Enter") === true) {
+    }
+  }
 }
 function checkSettingsWindow() {
   if (current_keys.get("x") === true) {
     if (settingRuned === false) {
-      paused = !paused
-      settingsWindowOpen = !settingsWindowOpen
-      settingRuned = true
+      paused = !paused;
+      settingsWindowOpen = !settingsWindowOpen;
+      settingRuned = true;
     }
   }
   if (current_keys.get("x") === false) {
-    settingRuned = false
+    settingRuned = false;
   }
 }
 function reset_ball() {
@@ -267,9 +326,9 @@ function reset_ball() {
   computer.posY = 265;
   ball.width = 10;
   ball.height = 10;
-//   if (settingsWindowOpen === false) {
-//     ball.alive = true;
-//   }
+  //   if (settingsWindowOpen === false) {
+  //     ball.alive = true;
+  //   }
   screenShaking = false;
   ball.x = 400;
   ball.y = 200;
@@ -293,19 +352,18 @@ function draw_score(ctx) {
 }
 let SoundedPlayed = false;
 function Flip_Controlls() {
-    if (FlipedControlles === true) {
-        if (SoundedPlayed === false) {
-            soundEFX3.play();
-            SoundedPlayed = true
-        }
+  if (FlipedControlles === true) {
+    if (SoundedPlayed === false) {
+      soundEFX3.play();
+      SoundedPlayed = true;
     }
-
+  }
 }
 function draw_paddles_and_ball(ctx) {
   ctx.fillStyle = "#ffeecc";
   ctx.imageSmoothingEnabled = false;
-  ctx.drawImage(PlayerPaddleSkin, player.posX, player.posY,14*2,70*2)
-  ctx.drawImage(PaddelSkinAI, computer.posX, computer.posY,14*2,70*2)
+  ctx.drawImage(PlayerPaddleSkin, player.posX, player.posY, 14 * 2, 70 * 2);
+  ctx.drawImage(PaddelSkinAI, computer.posX, computer.posY, 14 * 2, 70 * 2);
 
   for (let t = 0; t < 5; t++) {
     let per = t / 5;
@@ -382,11 +440,10 @@ function check_wall_collisions() {
   }
   if (player.posY <= 0) {
     player.posY = 0;
-
   }
   if (player.posY >= 580) {
     player.posY = 580;
-    }
+  }
 }
 function game_check_keyboard() {
   if (current_keys.get("ArrowRight") === true) {
@@ -413,26 +470,22 @@ function game_check_keyboard() {
   if (current_keys.get("Enter") === true) {
     splash = true;
   }
-  if (current_keys.get("w") || current_keys.get("ArrowUp") ) {
+  if (current_keys.get("w") || current_keys.get("ArrowUp")) {
     if (FlipedControlles === false) {
-        player.posY -= 5
+      player.posY -= 5;
     }
   }
   if (current_keys.get("s") || current_keys.get("ArrowDown")) {
-    if (FlipedControlles === false) [
-        player.posY += 5
-    ]
+    if (FlipedControlles === false) [(player.posY += 5)];
   }
-  if (current_keys.get("w") || current_keys.get("ArrowUp")){
+  if (current_keys.get("w") || current_keys.get("ArrowUp")) {
     if (FlipedControlles === true) {
-        console.log("TRUUUUE")
-        player.posY += 5
+      player.posY += 5;
     }
   }
   if (current_keys.get("s") || current_keys.get("ArrowDown")) {
     if (FlipedControlles === true) {
-        console.log("TRUUUUE")
-        player.posY -= 5
+      player.posY -= 5;
     }
   }
 }
@@ -470,7 +523,7 @@ function check_paddle_collisons() {
   if (
     new_ball_x >= 880 &&
     ball.y >= computer.posY &&
-    ball.y <= computer.posY + paddle.h*2
+    ball.y <= computer.posY + paddle.h * 2
   ) {
     ball.x = 880;
     ball.x_speed *= -1;
@@ -508,13 +561,12 @@ function update() {
   //check for user input
   game_check_keyboard(ctx);
 
-  updateLevelSelect(ctx,c);
-  checkSettingsWindow(ctx)
+  updateLevelSelect(ctx, c);
+  checkSettingsWindow(ctx);
 
-
-  if(paused) {
+  if (paused) {
     clear_screen(ctx);
-    draw_settings_window()
+    draw_settings_window();
   } else {
     if (ball.alive === true) {
       // move the computer
@@ -525,7 +577,7 @@ function update() {
       check_paddle_collisons(ctx);
       checkScore(ctx);
       update_particles(ctx);
-      Flip_Controlls(ctx)
+      Flip_Controlls(ctx);
       ctx.save();
       if (screenShaking === true) {
         ctx.translate(Math.random() * 30, 0);
